a1618e8813187d8924aee194bafb72a3a5fedc59
pokerfacer270 <Swidjaja270@gmail.com>
2022-02-17 23:58:05 -0800

    Potential UEFI for use

---
+++ PotentialUEFI/README.md
@@ -0,0 +1,2 @@
+# UEFI Application
+Simple EFI Application to say Hello, World. Will be adding more
---
+++ PotentialUEFI/gnu-efi/bootloader/CompLink.sh
@@ -0,0 +1,3 @@
+gcc -I../inc -fpic -ffreestanding -fno-stack-protector -fno-stack-check -fshort-wchar -mno-red-zone -maccumulate-outgoing-args -c main.c -o main.o
+ld -shared -Bsymbolic -L../x86_64/gnuefi -L../x86_64/lib -T../gnuefi/elf_x86_64_efi.lds ../x86_64/gnuefi/crt0-efi-x86_64.o main.o -o main.so -lgnuefi -lefi
+objcopy -j .text -j .sdata -j .data -j .dynamic -j .dynsym  -j .rel -j .rela -j .rel.* -j .rela.* -j .reloc --target efi-app-x86_64 --subsystem=10 main.so main.efi
