a69aaf76654ebbe0ddaaade1d34fbae507d3e3ca
pokerfacer270 <Swidjaja270@gmail.com>
2022-07-19 01:09:57 -0700

    Started Graphics, made python script for quick testing

---
+++ comp_and_run.py
@@ -0,0 +1,13 @@
+import os
+'''
+Sets of commands to compile and run on your system for testing. Change the commented lines for your OS
+Current System: Ubuntu 20.04
+'''
+
+os.chdir("/home/pokerfacer/SJSUOperatingSystem/PotentialUEFI/gnu-efi/") # Change based on your path and OS
+os.system("make bootloader") 
+os.chdir("/home/pokerfacer/SJSUOperatingSystem/PotentialUEFI/kernel") # Change based on your path and OS
+os.system("make kernel")
+os.system("make buildimg")
+os.system("make run")
+
