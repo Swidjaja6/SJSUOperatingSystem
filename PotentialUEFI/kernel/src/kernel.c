199fd7a546ec10fa1f270d0d1d41707d13830e7c
pokerfacer270 <Swidjaja270@gmail.com>
2022-08-14 21:26:35 -0700

    Progress before branch split

--- PotentialUEFI/kernel/src/kernel.c
+++ PotentialUEFI/kernel/src/kernel.c
@@ -1,3 +1,20 @@
-int _start() {
-    return 123;
+#include <stddef.h> 
+#include <stdint.h>
+
+typedef struct {
+        void* BaseAddress;
+        size_t BufferSize;
+        unsigned int Width;
+        unsigned int Height;
+        unsigned int PixelsPerScanLine;
+} Framebuffer;
+
+int _start(Framebuffer* framebuffer) {
+    unsigned int y = 50, BBP = 4;
+
+        for(unsigned int x = 0; x < framebuffer->Width / 2 * BBP; x += BBP) {
+                *(unsigned int*)(x + (y * framebuffer->PixelsPerScanLine * BBP) + framebuffer->BaseAddress) = 0xff00ffff;
+        }
+
+    return ;
 }
